a06641a08d360ab49e88e7337ee29f120f88e2d1
AminulIslamorange <aminulislamorange@gmail.com>
2022-11-30 15:33:32 +0600

    problem solve commit

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 *My live webSite Link:
+
+*for admin  email:admin@admin.com
+*password:123456
 *My website is about a resell car:
 *I have use React-Bootstart for Design.
 *I have use nodeJs for Backend site.
-*I host backend site On vercel.com
+*I host backend site On vercel.
+***Please Read carefully***
+When You go to dashboard..if this work fail then reload please..this problem for vercel...Thanks...
--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDcv7NfPIEjGFN-hDKc40t9W8ultJ-GRio",
-    authDomain: "carmania-client-main.firebaseapp.com",
-    projectId: "carmania-client-main",
-    storageBucket: "carmania-client-main.appspot.com",
-    messagingSenderId: "454484807557",
-    appId: "1:454484807557:web:338e1a398b3beedac1feec"
-  };
+  apiKey: "AIzaSyDcv7NfPIEjGFN-hDKc40t9W8ultJ-GRio",
+  authDomain: "carmania-client-main.firebaseapp.com",
+  projectId: "carmania-client-main",
+  storageBucket: "carmania-client-main.appspot.com",
+  messagingSenderId: "454484807557",
+  appId: "1:454484807557:web:338e1a398b3beedac1feec"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
